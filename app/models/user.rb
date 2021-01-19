876195623f6da41053b31a2489d3b6160298eb32
JaSON <masteraccounthub@gmail.com>
2021-01-19 05:18:12 -0500

    fixed webpacker fault

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :tips
     has_many :trick_tips, through: :tips, source: :tricks
     has_many :categories, through: :tricks
+
+    has_secure_password
 end
