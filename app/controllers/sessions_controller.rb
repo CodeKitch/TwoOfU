8c208b53e55b180c525068a4b5c81a760774be53
JaSON <masteraccounthub@gmail.com>
2021-01-19 22:13:49 -0500

    sessions more

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,15 @@ class SessionsController < ApplicationController
         redirect_to root_path
     end
 
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate([:user][:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+        else
+            flash[:message] = "Something didn't happen during login, please try again!"
+            redirect_to '/login'
+        end
+    end
+
 end
